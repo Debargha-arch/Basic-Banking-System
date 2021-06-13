21010d3235fa681dab42d649ef1ef3ca10bba160
Debargha-arch <mukherjeedebargha281@gmail.com>
2021-06-13 12:45:21 +0530

    New Change

--- config.php
+++ config.php
@@ -1,16 +1,11 @@
 <?php 
 
-//Development connection
-//$server="localhost";
-//$username="root";
-//$password="";
-//$db="bank_database";
+Development connection
+$server="localhost";
+$username="root";
+$password="";
+$db="bank_database";
 
-//Remote database connection
-$server="sql308.epizy.com";
-$username="epiz_28867932";
-$password="WHjD8eCgrwIuP";
-$db="epiz_28867932_bank_database";
 
 $conn=mysqli_connect($server,$username,$password,$db);
 
